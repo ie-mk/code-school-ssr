e64abfdaa449f76e7c368ce4832bcbe57ea4e50d
thisentirecitymustbepurged <yogintas@gmail.com>
2020-05-24 19:44:16 +0100

    attempt

--- src/next.config.js
+++ src/next.config.js
@@ -1,9 +1,12 @@
 const path = require('path');
 const withCSS = require('@zeit/next-css');
+const withFonts = require('next-fonts');
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 
 const projectRoot = path.join(__dirname);
+const MONACO_DIR = path.resolve(__dirname, '../node_modules/monaco-editor');
 
-module.exports = withCSS({
+module.exports = withFonts({
   distDir: '../.next',
   env: {
     useMockData: process.env.NODE_ENV === 'useMock',
@@ -16,6 +19,35 @@ module.exports = withCSS({
     alias['store'] = path.join(projectRoot, 'store');
     // alias['apiPath'] = path.join(projectRoot, '../api/api');
     alias['apiPath'] = path.join(projectRoot, './api/api.min');
+
+    config.plugins.push(
+      new MonacoWebpackPlugin({
+        // available options are documented at https://github.com/Microsoft/monaco-editor-webpack-plugin#options
+        languages: ['json'],
+      }),
+    );
+    config.module.rules[2] = {
+      test: /\.css$/,
+      include: MONACO_DIR,
+      use: ['style-loader', 'css-loader'],
+    };
+    config.module.rules.push({
+      test: /\.ttf$/,
+      include: MONACO_DIR,
+      use: [
+        {
+          loader: 'ttf-loader',
+          options: {
+            name: './font/[hash].[ext]',
+          },
+        },
+      ],
+    });
+
+    console.log('webpack -> config.module.rules', config.module.rules);
+    console.log('########################################');
+    console.log('########################################');
+
     return config;
   },
 });
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,21 +1,75 @@
-import React from 'react';
-import App from 'next/app';
-import withReduxStore from '../components/hoc/withReduxStore';
-import { Provider } from 'react-redux';
-import { logIsServer } from '../utils/utils';
-import AppBar from '../components/appBar/AppBar';
-import Footer from '../components/footer/Footer';
-import '../css/global.css';
-import Head from 'next/head';
-import SetLanguageFromStoreWrapper from '../i18n/SetLanguageFromStoreWrapper';
-import '../i18n/i18n';
-import ScrollTracker from '../components/foundation/scrollTracker/ScrollTracker';
+// import React from 'react';
+// import App from 'next/app';
+// import withReduxStore from '../components/hoc/withReduxStore';
+// import { Provider } from 'react-redux';
+// import { logIsServer } from '../utils/utils';
+// import AppBar from '../components/appBar/AppBar';
+// import Footer from '../components/footer/Footer';
+// import '../css/global.css';
+// import Head from 'next/head';
+// import SetLanguageFromStoreWrapper from '../i18n/SetLanguageFromStoreWrapper';
+// import '../i18n/i18n';
+// import ScrollTracker from '../components/foundation/scrollTracker/ScrollTracker';
 
-class MyApp extends App {
+// class MyApp extends App {
+//   static async getInitialProps({ Component, ctx }) {
+//     let pageProps = {};
+
+//     logIsServer('MyApp getInitialProps');
+
+//     if (Component.getInitialProps) {
+//       pageProps = await Component.getInitialProps(ctx);
+//     }
+
+//     return { pageProps };
+//   }
+
+//   render() {
+//     const { Component, pageProps, store } = this.props;
+//     logIsServer('MyApp');
+//     return (
+//       <>
+//         {/*<ThemeProvider theme={{ myTheme, orbit: customTokens }}>*/}
+//         <Head>
+//           <title>Rent a property</title>
+//           <link
+//             href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+//             rel="stylesheet"
+//           />
+//         </Head>
+//         <Provider store={store}>
+//           <SetLanguageFromStoreWrapper>
+//             <ScrollTracker>
+//               <AppBar />
+//               <main>
+//                 <Component {...pageProps} />
+//               </main>
+//               <Footer />
+//             </ScrollTracker>
+//           </SetLanguageFromStoreWrapper>
+//         </Provider>
+//         {/*</ThemeProvider>*/}
+//       </>
+//     );
+//   }
+// }
+
+// export default withReduxStore(MyApp);
+
+import React from 'react';
+import NextApp from 'next/app';
+import MonacoEditor from 'react-monaco-editor';
+
+export default class App extends NextApp {
+  constructor(props) {
+    super(props);
+    this.state = {
+      code: '// type your code...',
+    };
+  }
+
   static async getInitialProps({ Component, ctx }) {
     let pageProps = {};
-
-    logIsServer('MyApp getInitialProps');
 
     if (Component.getInitialProps) {
       pageProps = await Component.getInitialProps(ctx);
@@ -24,34 +78,31 @@ class MyApp extends App {
     return { pageProps };
   }
 
+  editorDidMount(editor, monaco) {
+    console.log('editorDidMount', editor, monaco);
+    editor.focus();
+  }
+
+  onChange(newValue, e) {
+    console.log('onChange', newValue, e);
+  }
+
   render() {
-    const { Component, pageProps, store } = this.props;
-    logIsServer('MyApp');
+    const code = this.state.code;
+
+    const options = {
+      selectOnLineNumbers: true,
+    };
+
     return (
-      <>
-        {/*<ThemeProvider theme={{ myTheme, orbit: customTokens }}>*/}
-        <Head>
-          <title>Rent a property</title>
-          <link
-            href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
-            rel="stylesheet"
-          />
-        </Head>
-        <Provider store={store}>
-          <SetLanguageFromStoreWrapper>
-            <ScrollTracker>
-              <AppBar />
-              <main>
-                <Component {...pageProps} />
-              </main>
-              <Footer />
-            </ScrollTracker>
-          </SetLanguageFromStoreWrapper>
-        </Provider>
-        {/*</ThemeProvider>*/}
-      </>
+      <MonacoEditor
+        language="javascript"
+        theme="vs-dark"
+        value={code}
+        options={options}
+        onChange={this.onChange}
+        editorDidMount={this.editorDidMount}
+      />
     );
   }
 }
-
-export default withReduxStore(MyApp);
