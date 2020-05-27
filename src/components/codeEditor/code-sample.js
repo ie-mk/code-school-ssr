eb388d5630a182491fe07b99197d9c5bc8a20c82
Justinas Simutis <justinas.simutis@gmail.com>
2020-05-27 14:06:27 +0100

    style update

--- src/components/codeEditor/code-sample.js
+++ src/components/codeEditor/code-sample.js
@@ -1,7 +1,17 @@
 export default `
     class Teletabis extends React.Component {
         render() {
-            return <div>Tinkis Vinkis</div>
+            return (
+              <div
+                style={{
+                  width: '100px',
+                  height: '100px',
+                  background: 'gray'
+                }}
+              >
+               Tinkis Vinkis
+              </div>
+            );
         }
     }
 `;
